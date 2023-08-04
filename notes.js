45380409d61a41d6a808a943e2780e676de3f40a
Adel Abdellatif <Adel.A.Abdelkader@gmail.com>
2023-08-04 14:48:43 +0300

    finish add Note functionality

--- notes.js
+++ notes.js
@@ -1,10 +1,26 @@
 var fs = require("fs");
 
+var fetchNotes = () => {
+  try {
+    return JSON.parse(fs.readFileSync("notes.txt"));
+  } catch (err) {
+    return [];
+  }
+};
+
 var addNote = function (title, body) {
+  let notes = fetchNotes();
   let note = { title, body };
 
-  fs.writeFileSync("notes.txt", JSON.stringify(note));
-  logNote(note);
+  let double = notes.filter((note) => note.title === title);
+
+  if (double.length > 0) {
+    console.log("Alert: Note Already Added");
+  } else {
+    notes.push(note);
+    fs.writeFileSync("notes.txt", JSON.stringify(notes));
+    logNote(note);
+  }
 };
 
 let logNote = function (note) {
